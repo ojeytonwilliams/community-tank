70f46c4dc4da89d18ce97955f625d21221f8599e
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-06-07 00:20:11 +0200

    fix: allow .lintstagedrc.js to be commited
    No need for --no-verify

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,25 @@
 const path = require("path");
+const { ESLint } = require("eslint");
 
-const buildEslintCommand = (filenames) =>
-  [`next lint --max-warnings=0  --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`, `prettier --write ${filenames}`];
+const cli = new ESLint();
+
+const buildEslintCommand = async (filenames) => {
+  const ignoredIds = await Promise.all(
+    filenames.map((file) => cli.isPathIgnored(file))
+  );
+  const lintableFiles = filenames.filter((_, i) => !ignoredIds[i]);
+
+  if (lintableFiles.length === 0) {
+    return `prettier --write ${filenames}`;
+  }
+
+  return [
+    `next lint --max-warnings=0  --fix --file ${lintableFiles
+      .map((f) => path.relative(process.cwd(), f))
+      .join(" --file ")}`,
+    `prettier --write ${filenames}`,
+  ];
+};
 
 module.exports = {
   "*.+(j|t)s?(x)": [buildEslintCommand],
